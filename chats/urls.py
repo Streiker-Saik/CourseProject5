aefc00e3c976c9b0ee4f44ccceb52e793fb4227e
Alexei <streiker.saik@gmail.com>
2025-08-25 23:55:05 +0300

    add CRUD Habit + urls

--- chats/urls.py
+++ chats/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
+from chats.views import HabitCreateAPIView, HabitListAPIView, HabitRetrieveAPIView, HabitUpdateAPIView, \
+    HabitDestroyAPIView, PublicHabitListAPIView
 from users.apps import UsersConfig
 
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path("habits/my/", HabitListAPIView.as_view(), name="my-habits"),
+    path("habits/public/", PublicHabitListAPIView.as_view(), name="public-habits"),
     # CRUD Habit
-
+    path("habits/create/", HabitCreateAPIView.as_view(), name="habit-create"),
+    path("habits/<int:pk>/", HabitRetrieveAPIView.as_view(), name="habit-detail"),
+    path("habits/<int:pk>/update/", HabitUpdateAPIView.as_view(), name="habit-update"),
+    path("habits/<int:pk>/delete/", HabitDestroyAPIView.as_view(), name="habit-delete"),
 ]
